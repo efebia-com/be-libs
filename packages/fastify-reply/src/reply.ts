38bafb981bed0fffec8d41c7bf7766066cc8ff19
HiiiiD <salomone.marco@outlook.it>
2023-01-12 16:22:28 +0100

    Create fastify-reply

---
+++ packages/fastify-reply/src/reply.ts
@@ -0,0 +1,39 @@
+import { FastifyReply } from "fastify";
+import { name } from "../package.json";
+
+type ReplyFunction<T> = T extends (...args: any[]) => any
+  ? (this: FastifyReply, ...args: Parameters<T>) => ReturnType<T>
+  : never;
+
+export const createReply = (
+  statusCode: number,
+  defaultPayload: object
+): ReplyFunction<any> => {
+  return function (payload: any) {
+    const finalPayload = payload ?? defaultPayload;
+    if (!/^2/u.test(statusCode.toString()))
+      throw createError(statusCode)(finalPayload as any);
+    this.type("application/json");
+    this.code(statusCode);
+    const serialized = this.serialize(finalPayload);
+    if (serialized == `"[object Object]"`)
+      throw new Error(`${name}: Response schema didn't expect an object`);
+    this.send(serialized);
+  };
+};
+
+export const createError =
+  (statusCode: number) =>
+  (message: string | { statusCode: number; message: string }) => {
+    const customError: Error & { statusCode?: number } = new Error();
+    if (typeof message === "string") {
+      customError.statusCode = statusCode;
+      customError.message = message;
+      return customError;
+    }
+
+    customError.statusCode = message.statusCode;
+    customError.message = message.message;
+
+    return customError;
+};
