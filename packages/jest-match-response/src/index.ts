65966f67dda9ada67dc4dc0205c6c9be55c37603
HiiiiD <salomone.marco@outlook.it>
2023-09-12 12:09:16 +0200

    Fix matchResponseAndReturn type definition

--- packages/jest-match-response/src/index.ts
+++ packages/jest-match-response/src/index.ts
@@ -1,13 +1,13 @@
 export { buildToMatchJson, type Options } from "./toMatchJson";
 export { toMatchResponse } from "./toMatchResponse";
 
-export const matchResponseAndReturn = async (response: { json: () => any | Promise<any> } & ({ status: number } | { statusCode: number }), model: any) => {
+export const matchResponseAndReturn = async (response: { json: () => any | Promise<any> } & ({ status: number } | { statusCode: number }), statusCode: number, model: any) => {
     const jsonValue = await response.json();
     expect({ ...response, json() {
         return jsonValue
     }})
     //@ts-ignore
-    .toMatchResponse(model);
+    .toMatchResponse(statusCode, model);
 
     return jsonValue;
 }
