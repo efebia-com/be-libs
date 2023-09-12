e661731e87386522b943ad2599bc8eaa7816e270
HiiiiD <salomone.marco@outlook.it>
2023-09-12 12:06:41 +0200

    Fix toMatchResponse and add matchResponseAndReturn

--- packages/jest-match-response/src/index.ts
+++ packages/jest-match-response/src/index.ts
@@ -1,3 +1,13 @@
 export { buildToMatchJson, type Options } from "./toMatchJson";
 export { toMatchResponse } from "./toMatchResponse";
 
+export const matchResponseAndReturn = async (response: { json: () => any | Promise<any> } & ({ status: number } | { statusCode: number }), model: any) => {
+    const jsonValue = await response.json();
+    expect({ ...response, json() {
+        return jsonValue
+    }})
+    //@ts-ignore
+    .toMatchResponse(model);
+
+    return jsonValue;
+}
--- packages/jest-match-response/src/toMatchResponse.ts
+++ packages/jest-match-response/src/toMatchResponse.ts
@@ -1,29 +1,34 @@
 import { expect } from '@jest/globals';
+import type { MatcherFunction } from "expect";
 
-export const toMatchResponse = async function(received, statusCode, model) {
-    if (!(Object.hasOwn(received, 'json') && typeof received.json === 'function')) throw new Error('toMatchResponse: the received value needs to have a .json method');
-    if (!(Object.hasOwn(received, 'status') || Object.hasOwn(received, 'statusCode'))) throw new Error('toMatchResponse: the received value needs to have a status or statusCode property');
-    const status = received.status || received.statusCode;
+export const toMatchResponse: MatcherFunction<[number, any]> = async function(received, statusCode, model) {
+    if (typeof received !== 'object' || !received) throw new Error('toMatchResponse: the received value needs to be of type object');
+    if (!('json' in received && typeof received.json === 'function')) throw new Error('toMatchResponse: the received value needs to have a .json method');
+    if (!('status' in received || 'statusCode' in received)) throw new Error('toMatchResponse: the received value needs to have a status or statusCode property');
+    const status = ('status' in received ? received.status : received.statusCode);
     const jsonResponse = await received.json();
 
     const statusCodePass = this.isNot ? status !== statusCode : status === statusCode;
+    const message = () => (
+      // eslint-disable-next-line prefer-template
+      this.utils.matcherHint('toMatchResponse', undefined, undefined, { comment: 'Match response equality', isNot: this.isNot, promise: this.promise }) +
+      '\n\n' +
+      `Expected Status Code: ${this.utils.printExpected(statusCode)}\n` +
+      `Received Status Code: ${this.utils.printReceived(status)}\n` +
+      `Received Body: ${this.utils.printReceived(jsonResponse)}`
+    );
     if (!statusCodePass) {
-        const message = () => (
-            // eslint-disable-next-line prefer-template
-            this.utils.matcherHint('toMatchResponse', undefined, undefined, { comment: 'Match response equality', isNot: this.isNot, promise: this.promise }) +
-            '\n\n' +
-            `Expected Status Code: ${this.utils.printExpected(statusCode)}\n` +
-            `Received Status Code: ${this.utils.printReceived(status)}\n` +
-            `Received Body: ${this.utils.printReceived(jsonResponse)}`
-        );
-
         return { actual: status, message, name: 'toMatchResponse', pass: false };
     }
 
-    if (this.isNot) return expect(jsonResponse).not.toMatchJson(model);
+    if (this.isNot) {
+      expect(jsonResponse).not.toMatchJson(model);
+      return { pass: true, name: 'toMatchResponse', message }
+    }
+
     expect(jsonResponse).toMatchJson(model)
 
-    return { pass: true }
+    return { pass: true, name: 'toMatchResponse', message }
 };
 
 declare module "expect" {
