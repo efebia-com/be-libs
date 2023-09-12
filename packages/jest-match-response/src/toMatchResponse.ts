c004ac7700c9f1ae3535cdd5f5aa4602d14dce4d
HiiiiD <salomone.marco@outlook.it>
2023-09-12 10:10:13 +0200

    Make toMatchResponse more flexible

--- packages/jest-match-response/src/toMatchResponse.ts
+++ packages/jest-match-response/src/toMatchResponse.ts
@@ -1,21 +1,23 @@
 import { expect } from '@jest/globals';
 
 export const toMatchResponse = async function(received, statusCode, model) {
-    if (!(received instanceof Response)) throw new Error('toMatchResponse: the received value has to be a fetch Response object')
+    if (!(Object.hasOwn(received, 'json') && typeof received.json === 'function')) throw new Error('toMatchResponse: the received value needs to have a .json method');
+    if (!(Object.hasOwn(received, 'status') || Object.hasOwn(received, 'statusCode'))) throw new Error('toMatchResponse: the received value needs to have a status or statusCode property');
+    const status = received.status || received.statusCode;
     const jsonResponse = await received.json();
 
-    const statusCodePass = this.isNot ? received.status !== statusCode : received.status === statusCode;
+    const statusCodePass = this.isNot ? status !== statusCode : status === statusCode;
     if (!statusCodePass) {
         const message = () => (
             // eslint-disable-next-line prefer-template
             this.utils.matcherHint('toMatchResponse', undefined, undefined, { comment: 'Match response equality', isNot: this.isNot, promise: this.promise }) +
             '\n\n' +
             `Expected Status Code: ${this.utils.printExpected(statusCode)}\n` +
-            `Received Status Code: ${this.utils.printReceived(received.status)}\n` +
+            `Received Status Code: ${this.utils.printReceived(status)}\n` +
             `Received Body: ${this.utils.printReceived(jsonResponse)}`
         );
 
-        return { actual: received.status, message, name: 'toMatchResponse', pass: false };
+        return { actual: status, message, name: 'toMatchResponse', pass: false };
     }
 
     if (this.isNot) return expect(jsonResponse).not.toMatchJson(model);
